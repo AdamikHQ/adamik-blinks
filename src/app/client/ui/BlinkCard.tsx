f0d8ca743648b2c125bec8e92d2b988311d1d223
hakim-adamik <hakim@adamik.io>
2024-09-21 20:02:58 +0800

    Add chain icons to Blink UI

--- src/app/client/ui/BlinkCard.tsx
+++ src/app/client/ui/BlinkCard.tsx
@@ -2,19 +2,30 @@
 
 import { BlinkConfig } from "~/types/blinks";
 import { amountToMainUnit } from "../logic/utils";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Chain } from "~/types/adamik";
+
+import bitcoinIcon from "./bitcoin.svg";
+import atomIcon from "./atom.svg";
+import baseIcon from "./base.svg";
+import Image from "next/image";
 
 type BlinkCardProps = {
   config: BlinkConfig;
-  action: () => void;
   chain: Chain | undefined;
-  //ticker?: string;
-  //donateValue?: number[] | string;
-  //donateRedirectUrl?: string;
+  action: () => void;
+  amount: string | undefined;
+  setTransactionAmount: (amount: string) => void;
 };
 
-export const BlinkCard = ({ config, action, chain }: BlinkCardProps) => {
+export const BlinkCard = ({
+  config,
+  action,
+  chain,
+  amount,
+  setTransactionAmount,
+}: BlinkCardProps) => {
+  /*
   const formattedAmount = useMemo(
     () =>
       chain?.decimals
@@ -22,6 +33,7 @@ export const BlinkCard = ({ config, action, chain }: BlinkCardProps) => {
         : undefined,
     [config.transactionData.amount, chain]
   );
+  */
 
   return !chain ? null : (
     <div className="flex">
@@ -44,57 +56,63 @@ export const BlinkCard = ({ config, action, chain }: BlinkCardProps) => {
           {config.metadata.description}
         </div>
 
+        <div className="flex py-2 gap-1">
+          <Image src={baseIcon} alt="base" width={32} height={32} />
+          <Image src={bitcoinIcon} alt="bitcoin" width={32} height={32} />
+          <Image src={atomIcon} alt="atom" width={32} height={32} />
+        </div>
+
+        {/*
         <>
           <button
             className="w-full py-2 mt-4 text-sm font-semibold uppercase text-white bg-blue-600  hover:bg-blue-500 rounded-3xl"
             onClick={action}
           >
-            {/* FIXME Hack, to be reverted */}
-            {/* {formattedAmount} {chain?.ticker} */}
-            2.5 USD
+            {formattedAmount} {chain?.ticker}
           </button>
         </>
+        */}
 
         {/*
-      <div className="flex justify-between gap-3">
-        {typeof donateValue === "string" ? (
-          <>
-            <button className="w-full py-2 mt-4 text-sm font-semibold uppercase text-white bg-blue-600  hover:bg-blue-500 rounded-3xl">
-              {donateValue}
-            </button>
-          </>
-        ) : (
-          donateValue.map((value, i) => {
-            return (
-              <button
-                key={`${value}_${i}`}
-                className="w-full py-2 mt-4 text-sm font-semibold uppercase text-white bg-blue-600  hover:bg-blue-500 rounded-3xl"
-              >
-                {value} {ticker}
+        <div className="flex justify-between gap-3">
+          {typeof donateValue === "string" ? (
+            <>
+              <button className="w-full py-2 mt-4 text-sm font-semibold uppercase text-white bg-blue-600  hover:bg-blue-500 rounded-3xl">
+                {donateValue}
               </button>
-            );
-          })
-        )}
-      </div>
-      */}
+            </>
+          ) : (
+            donateValue.map((value, i) => {
+              return (
+                <button
+                  key={`${value}_${i}`}
+                  className="w-full py-2 mt-4 text-sm font-semibold uppercase text-white bg-blue-600  hover:bg-blue-500 rounded-3xl"
+                >
+                  {value} {ticker}
+                </button>
+              );
+            })
+          )}
+        </div>
+        */}
 
-        {/*
         <div className="relative mt-3">
           <input
             type="text"
             className="h-14 w-full pl-4 pr-10 rounded-3xl border-slate-500/40 border-[1px] bg-stone-900 focus:border-blue-600  focus:outline-none "
-            placeholder={`Enter a custom ${ticker} amount`}
+            placeholder={`Enter a custom ${chain?.ticker} amount`}
+            onInput={(e) => setTransactionAmount(e.currentTarget.value)}
           />
           <div className="absolute top-2 right-2">
             <button
-              disabled
+              disabled={amount !== undefined}
               className="h-10 w-24 text-white rounded-3xl bg-blue-600 hover:bg-blue-500 disabled:bg-stone-800 disabled:text-stone-500"
+              onClick={action}
             >
               Donate
             </button>
           </div>
         </div>
-        */}
       </div>
     </div>
   );
